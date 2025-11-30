3bf5160d1e57b132a92cb20aa0f6bbb8092bb235
Mila1213 <tu_correo@ejemplo.com>
2025-11-30 09:20:54 -0500

    Estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import Integrantes from './components/Integrantes/Integrantes.jsx';
+import Integrantes from './components/integrantes/Integrantes.jsx';
 import About from './components/about/About.jsx';
 import Titulo from './components/titulo/Titulo.jsx';
 import Conocimientos from './components/Conocimientos/Conocimientos.jsx';
-import Imagen from './components/Imagen/Imagen.jsx';
-
 function App() {
   return (
     <>
     <Titulo/>
     <Conocimientos/>
-    <Imagen/>
     <Integrantes />
     <About />
     </>
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,19 +1,28 @@
+// About.jsx
+import React from 'react';
 import './About.css';
+import imagenLateral from '../../assets/imagen2.png'; // ajusta la ruta si la tienes en otro sitio
 
 const About = () => {
-    return (
-        <section id="about" className="about">
-            <div className="about__content">
-                <h2 className="about__title">Sobre Nosotros</h2>
-                <p className="about__paragraph">
-                    Somos un grupo de estudiantes de desarrollo de Software en donde cada integrante aporta sus habilidades y conocimientos para trabajar de la mejor manera colaborativa, desde el diseño de interfaces y la programación hasta organización del contenido y la experiencia del usuario.
-                </p>
-                <p className="about__paragraph">
-                    Nuestro objetivo es poder transmitir nuestro compromiso con la calidad y el potencial en el ámbito digital y creativo a través de una página web.
-                </p>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="about" className="about">
+      <div className="about__content">
+        <div className="about__text">
+          <h2 className="about__title">Sobre Nosotros</h2>
+          <p className="about__paragraph">
+            Somos un grupo de estudiantes de desarrollo de Software en donde cada integrante aporta sus habilidades y conocimientos para trabajar de la mejor manera colaborativa, desde el diseño de interfaces y la programación hasta organización del contenido y la experiencia del usuario.
+          </p>
+          <p className="about__paragraph">
+            Nuestro objetivo es poder transmitir nuestro compromiso con la calidad y el potencial en el ámbito digital y creativo a través de una página web.
+          </p>
+        </div>
 
-export default About
+        <div className="about__imageWrapper">
+          <img src={imagenLateral} alt="programador" className="about__image" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
