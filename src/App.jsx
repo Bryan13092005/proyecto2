f80a9899617af535a3495a0f72776e6d672f3c21
Bryan13092005 <bryan.salvador@epn.edu.ec>
2025-12-19 22:03:25 -0500

    Imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Login from './components/login/Login';
 import Register from './components/register/Register.jsx';
 import Landing from './components/landing/Landing';
 
+const basename =
+  window.location.hostname.includes("github.io")
+    ? "/proyecto2"
+    : "/";
+
 function App() {
   return (
-
-<BrowserRouter basename="/proyecto2">
+    <BrowserRouter basename={basename}>
   <Routes>
     <Route index element={<Landing />} />
     <Route path="login" element={<Login />} />
