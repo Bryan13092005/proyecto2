27adeab1a545e9daf973758f02978ae59b65fc33
Jossue Chulde <fredderick.chulde@epn.edu.ec>
2025-12-20 19:14:19 -0500

    Importando nueva imagen al componente

--- src/components/ImagenesEfectos/ImagenesEfectos.jsx
+++ src/components/ImagenesEfectos/ImagenesEfectos.jsx
@@ -5,6 +5,7 @@ import img2 from "../../assets/c++.jpg";
 import img3 from "../../assets/Analisis.jpg";
 import img4 from "../../assets/IA.jpg";
 import img5 from "../../assets/programacion.jpg";
+import img6 from "../../assets/códigos.jpg";
 
 function ImagenesEfectos() {
   return (
@@ -14,6 +15,7 @@ function ImagenesEfectos() {
         <img src={img3} alt="Analisis" className='imagen analisis'></img>
         <img src={img4} alt="Inteligencia Artificial" className='imagen ia'></img>
         <img src={img5} alt="Programacion" className='imagen programacion'></img>
+        <img src={img6} alt="Códigos" className='imagen codigos'></img>
     </section>
   )
 }
